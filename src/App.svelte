85cb307d6f82edd0478c9815386bc9b5351a4636
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-01-03 11:43:46 +0545

    Add empty list component

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import { Router, Route } from "svelte-routing";
 
   import Main from "./views/Main.svelte";
-  import TodoList from "./views/TodoList.svelte"
+  import TodoList from "./views/TodoList.svelte";
   import Search from "./components/Search.svelte";
 
   export let searchText = "";
@@ -10,13 +10,46 @@
   function handleInput(event) {
     searchText = event.detail.value;
   }
+
+  export let list = [
+    { id: 1, title: "A random todo item", completed: false },
+    { id: 2, title: "Another random todo item", completed: true }
+  ];
+
+  function searchList(list = [], searchText = "") {
+    return list.filter(item =>
+      item.title.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }
+
+  function filterByCompleted(list = [], completed) {
+    return list.filter(item => item.completed === completed);
+  }
+
+  function toggleItem(event) {
+    const index = event.detail.index;
+
+    const item = list.find(listItem => listItem.id === index);
+
+    list[list.indexOf(item)].completed = !item.completed;
+  }
 </script>
 
 <Router>
   <Main>
     <Search on:input={handleInput} />
-    <Route path="/completed">Completed</Route>
-    <Route path="/remaining">Remaining</Route>
-    <Route path="/"><TodoList>Home</TodoList></Route>
+    <Route path="/completed">
+      <TodoList
+        list={filterByCompleted(searchList(list, searchText), true)}
+        on:itemClick={toggleItem} />
+    </Route>
+    <Route path="/remaining">
+      <TodoList
+        list={filterByCompleted(searchList(list, searchText), false)}
+        on:itemClick={toggleItem} />
+    </Route>
+    <Route path="/">
+      <TodoList list={searchList(list, searchText)} on:itemClick={toggleItem} />
+    </Route>
   </Main>
 </Router>
--- src/views/TodoList.svelte
+++ src/views/TodoList.svelte
@@ -1,31 +1,38 @@
 <script>
-import TodoItem from '../components/TodoItem.svelte';
-import Container from '../components/Container.svelte';
+  import EmptyList from "./EmptyList.svelte";
+  import TodoItem from "../components/TodoItem.svelte";
+  import Container from "../components/Container.svelte";
 
-export let list = [
-  {title: 'A random todo item', completed: false},
-  {title: 'Another random todo item', completed: true}
-];
+  export let list = [];
 
-function toggleItem(index) {
-  list[index].completed = !list[index].completed;
-}
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  // Event Handlers
+  function handleClick(index) {
+    dispatch("itemClick", { index });
+  }
 </script>
 
 <style>
-.todo-list-container {
-  background: #fff;
-  overflow: hidden;
-  border-radius: 3px;
-  margin: 32px -20px 0;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.25);
-}
+  .todo-list-container {
+    background: #fff;
+    overflow: hidden;
+    border-radius: 3px;
+    margin: 32px -20px 0;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.25);
+  }
 </style>
 
 <Container>
   <div class="todo-list-container">
-    {#each list as {title, completed}, i}
-      <TodoItem bind:title bind:completed on:click={() => toggleItem(i)} />
-    {/each}
+    {#if list.length > 0}
+      {#each list as { id, title, completed }}
+        <TodoItem bind:title bind:completed on:click={() => handleClick(id)} />
+      {/each}
+    {:else}
+      <EmptyList />
+    {/if}
   </div>
 </Container>
